deda1c79a0a9ff0bb4f5186ade20043b46c43537
Aquiles Viza <aquiles.viza@sansano.usm.cl>
2022-12-10 00:12:05 -0300

    Tres archivos de desarrollo, buscando refinar el algoritmo

--- tarea2/algorithms.py
+++ tarea2/algorithms.py
@@ -39,11 +39,11 @@ def north_west_heuristic(oferta_i: NDArray, demanda_j: NDArray, costos_ij: NDArr
     oferta_actual = oferta_i[i,0]
     demanda_actual = demanda_j[j,0]
 
-    print(f"{   oferta_actual = }")
-    print(f"{   demanda_actual = }")
+    # print(f"{   oferta_actual = }")
+    # print(f"{   demanda_actual = }")
 
     if oferta_actual == demanda_actual:
-      print("   - iguales")
+      # print("   - iguales")
       factores_ij[i,j] = oferta_actual
       
       oferta_i[i,0] -= oferta_actual
@@ -53,7 +53,7 @@ def north_west_heuristic(oferta_i: NDArray, demanda_j: NDArray, costos_ij: NDArr
       dj = 1
       
     elif oferta_actual >= demanda_actual:
-      print("   - demanda_actual es menor")
+      # print("   - demanda_actual es menor")
       factores_ij[i,j]=demanda_actual
 
       oferta_i[i,0]  -= demanda_actual
@@ -64,7 +64,7 @@ def north_west_heuristic(oferta_i: NDArray, demanda_j: NDArray, costos_ij: NDArr
 
 
     elif oferta_actual <= demanda_actual:
-      print("   - oferta_actual es menor")
+      # print("   - oferta_actual es menor")
       factores_ij[i,j]=oferta_actual
 
       oferta_i[i,0]  -= oferta_actual
@@ -74,14 +74,77 @@ def north_west_heuristic(oferta_i: NDArray, demanda_j: NDArray, costos_ij: NDArr
       dj = 0
 
     if oferta_i[i,0] == 0 and demanda_j[j,0] == 0:
-      print("Se llega al equilibrio")
+      # print("Se llega al equilibrio")
       return factores_ij
     
-    print(oferta_i)
-    print(demanda_j)
+    # print(oferta_i)
+    # print(demanda_j)
     i += di
     j += dj
 
-    print()
+    # print()
 
 
+def costos_reducidos(idx_B: NDArray, idx_NB: NDArray, A: NDArray, c: NDArray) -> NDArray:
+  c_B = c[idx_B]
+  A_B = A[:, idx_B]
+  c_NB = c[idx_NB]
+  A_NB = A[:, idx_NB]
+  
+  return c_NB - np.linalg.multi_dot( [c_B,  np.linalg.inv(A_B) , A_NB])
+
+
+
+def es_factible(X_B):
+  return (X_B >= 0).all()
+
+def es_optimo(cr):
+
+  if (cr >= 0).all():
+    print("Tenemos un óptimo")
+
+    if (cr == 0).any():
+      print("Son múltiples óptimos")
+    else:
+      print("Es único")
+
+    return True
+
+  return False
+
+def direccion_costo_reducido(X: NDArray, A: NDArray, B:NDArray, NB: NDArray, c: NDArray, NB_i: int) -> NDArray:
+  d = np.zeros(shape=X.shape)
+  d_B = - np.dot(
+    np.linalg.inv(A[:,B]),
+    A[:, NB_i]
+  )
+
+  np.put(d, NB_i, 1)
+  np.put(d, B, d_B)
+
+
+  cr = c[NB_i] + np.dot(c[B].T, d[B])
+
+  # i, d, cr
+  return NB_i, d, cr.flatten()
+
+def lista_direccion_costos_reducidos(X: NDArray, A: NDArray, c: NDArray, B:NDArray, NB: NDArray) -> NDArray:
+
+  d_cr_list = list()
+
+  for NB_i in NB:
+    d = np.zeros(shape=X.shape)
+    d_B = - np.dot(
+      np.linalg.inv(A[:,B]),
+      A[:, NB_i]
+    )
+
+    np.put(d, NB_i, 1)
+    np.put(d, B, d_B)
+
+
+    cr = c[NB_i] + np.dot(c[B].T, d[B])
+
+    d_cr_list.append((NB_i, d, cr.flatten()))
+
+  return d_cr_list
